2ec2675832c69067a8a32acc92693752c401851b
Stephen Eldridge <eldridge.sje@gmail.com>
2021-12-11 14:21:49 -0600

    Populated dropdown.

--- static/js/plots.js
+++ static/js/plots.js
@@ -1,1 +1,31 @@
+// Code begun with Dom's office hours code from 12/11/21
+
 console.log("plots.js is loaded");
+
+function InitDashboard()
+{
+    console.log("Initializing the dashboard...");
+
+    let selector = d3.select("#selDataset");
+
+    d3.json("../samples.json").then(data => {
+
+        console.log(data);
+
+        let sampleIDs = data.names;
+
+        sampleIDs.forEach(ID => {
+            
+            selector.append("option")
+                .text(ID)
+                .property("value", ID);
+
+        });
+
+    })
+
+
+
+}
+
+InitDashboard();
