ca0ed4e88beae3de8515c6402c6b77933a8c78a2
Stephen Eldridge <eldridge.sje@gmail.com>
2021-12-11 21:50:47 -0600

    Got bubble chart up and running.

--- static/js/plots.js
+++ static/js/plots.js
@@ -1,12 +1,8 @@
 // Code begun with Dom's office hours demonstration from 12/11/21
 
-console.log("plots.js is loaded");
-
 // FUNCTION FOR THE BAR CHART
 
 function barChart(sampleID) {
-
-    console.log(`Draw barchart for (${sampleID})`);
 
     d3.json("samples.json").then(data => {
 
@@ -22,13 +18,9 @@ function barChart(sampleID) {
 
         let yticks = otu_ids.slice(0, 10).map(id => `OTU ${id}`);
 
-        console.log(otu_ids)
-        console.log(otu_labels)
-        console.log(sample_values)
-
         let plotData = {
             x: sample_values.slice(0, 10).reverse(),
-            y: yticks,
+            y: yticks.reverse(),
             type: "bar",
             text: otu_labels.slice(0, 10).reverse(),
             orientation: "h"
@@ -37,11 +29,11 @@ function barChart(sampleID) {
         let barArray = [plotData];
 
         let barLayout = {
-            Title: "Top 10 Bacteria Cultures Found",
-            margin: {t: 30, 1: 50}
+            title: "Top 10 Bacteria Cultures Found",
+            margin: {t: 30, 1: 10}
         }
 
-    Plotly.newPlot("bar", barArray);
+    Plotly.newPlot("bar", barArray, barLayout);
 
 
     })
@@ -53,7 +45,44 @@ function barChart(sampleID) {
 
 function bubbleChart(sampleID) {
 
-    console.log(`Draw bubblechart for (${sampleID})`);
+    d3.json("samples.json").then(data => {
+
+        let samples = data.samples;
+
+        let filterData = samples.filter(s => s.id === sampleID);
+
+        let result = filterData[0]
+
+        console.log(filterData);
+        console.log(result);
+
+        let otu_ids = result.otu_ids;
+        let otu_labels = result.otu_labels;
+        let sample_values = result.sample_values;
+
+        let plotData = {
+            x: otu_ids,
+            y: sample_values,
+            mode: "markers",
+            marker: {
+                size: sample_values,
+                color: otu_ids,
+                colorscale: "Earth"
+            },
+            text: otu_labels
+        };
+
+        let barArray = [plotData];
+
+        let barLayout = {
+            title: "Bacteria Cultures Per Sample",
+            margin: {t: 30, 1: 10}
+        }
+
+    Plotly.newPlot("bubble", barArray, barLayout);
+
+
+    })
 
 }
 
@@ -62,7 +91,6 @@ function bubbleChart(sampleID) {
 
 function demoBox(sampleID) {
 
-    console.log(`Populate demographics for (${sampleID})`);
 
 }
 
@@ -70,8 +98,6 @@ function demoBox(sampleID) {
 // FUNCTION FOR UPDATING BASED ON USER SELECTION
 
 function optionChanged(id) {
-
-    console.log(`An option was changed! (${id})`)
 
     barChart(id)
 
@@ -85,13 +111,10 @@ function optionChanged(id) {
 
 
 function InitDashboard() {
-    console.log("Initializing the dashboard...");
 
     let selector = d3.select("#selDataset");
 
     d3.json("../samples.json").then(data => {
-
-        console.log(data);
 
         let sampleIDs = data.names;
 
